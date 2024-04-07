920fa0cb3fd23acf713d1286a74f1ebe3eda5288
Jonathan Bell <j.bell@u.northwestern.edu>
2024-04-07 05:15:25 -0500

    pts working, need to add relative posn tho

--- root/src/components/Dropdown.jsx
+++ root/src/components/Dropdown.jsx
@@ -9,8 +9,6 @@ export default function Dropdown() {
                 <div className="dropdown-content">
                     <Link to="/b">Basement</Link>
                     <Link to="/g">Ground</Link>
-                    <Link to="/1">Floor 1</Link>
-                    <Link to="/2">Floor 2</Link>
                 </div>
             </div>
             <Outlet />
