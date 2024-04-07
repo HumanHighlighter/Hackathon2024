97497a09ff8e31eb1ac495b3beeeb6073435268a
Jonathan Bell <j.bell@u.northwestern.edu>
2024-04-07 01:38:51 -0500

    dog pooooo

--- root/src/App.jsx
+++ root/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'; 
 import Dropdown from './components/Dropdown.jsx';
 import Basement from './components/Maps/Basement.jsx';
+import Ground from './components/Maps/Ground.jsx';
+import First from './components/Maps/First.jsx';
+import Second from './components/Maps/Second.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Dropdown />}>
               <Route path="b" element={<Basement />} />
+              <Route path="g" element={<Ground />} />
+              <Route path="1" element={<First />} />
+              <Route path="2" element={<Second />} />
             </Route>
           </Routes>
         </BrowserRouter>
--- root/src/components/Maps/Basement.jsx
+++ root/src/components/Maps/Basement.jsx
@@ -1,12 +1,23 @@
 import './Maps.css';
 import basement from './basement.png';
+import Modal from './Modal.jsx';
+import { useState } from 'react';
 
 export default function Basement() {
+    // const [pts, setPts] = useState([]);
+    const [active, setActive] = useState(false);
+
+
     return (
         <>
             <div className="img-container">
                 <img src={basement} alt={"basement"} />
-                <div className="test"></div>
+                {/* <div className="test"></div> */}
+                <button onClick={() => setActive(!active)}>MODAL BUTTON</button>
+
+                <Modal open={active} onClose={() => setActive(false)}>
+                    hello world
+                </Modal>
             </div>
         </>
     );
