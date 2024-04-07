9912621e021a8432056777ce5624a09ebe19216c
Jonathan Bell <j.bell@u.northwestern.edu>
2024-04-07 03:40:17 -0500

    working modal

--- root/src/components/Maps/Basement.jsx
+++ root/src/components/Maps/Basement.jsx
@@ -3,21 +3,32 @@ import basement from './basement.png';
 import Modal from './Modal.jsx';
 import { useState } from 'react';
 
+
 export default function Basement() {
+
+
     // const [pts, setPts] = useState([]);
     const [active, setActive] = useState(false);
+
+    const pt_pos = {
+        transform: "translate(50px, -60px)"
+    };
 
 
     return (
         <>
             <div className="img-container">
                 <img src={basement} alt={"basement"} />
-                {/* <div className="test"></div> */}
-                <button onClick={() => setActive(!active)}>MODAL BUTTON</button>
+                <button className="pt" style={pt_pos} onClick={() => setActive(true)}></button>
 
-                <Modal open={active} onClose={() => setActive(false)}>
-                    hello world
-                </Modal>
+                <Modal 
+                    open={active} 
+                    onClose={() => setActive(false)} 
+                    wing="M"
+                    floor="2"
+                    status="Green"
+                    quality="3.2"
+                />
             </div>
         </>
     );
--- root/src/components/Maps/Modal.jsx
+++ root/src/components/Maps/Modal.jsx
@@ -1,13 +1,29 @@
-import ReactDom from 'react-dom';
+// from https://www.youtube.com/watch?v=LyLa7dU5tp8&ab_channel=WebDevSimplified
 
-export default function Modal({ open, onClose, children }) {
+import ReactDom from 'react-dom';
+import './Modal.css';
+import svg from './x-symbol-svgrepo-com.svg';
+
+export default function Modal({ open, onClose, wing, floor, status, quality }) {
     const modal_styles = {
-        position: "absolute",
-        top: "0px",
-        left: "0px",
-        transform: "translate(0px, 0px)",
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
         backgroundColor: "#FFF",
+        borderRadius: "6px",
         padding: "50px",
+        paddingTop: "5px",
+        zIndex: 1000
+    };
+
+    const overlay_styles = {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0, 
+        bottom: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.7)",
         zIndex: 1000
     };
 
@@ -15,9 +31,14 @@ export default function Modal({ open, onClose, children }) {
 
     return ReactDom.createPortal(
         <>
+            <div style={overlay_styles}></div>
             <div style={modal_styles}>
-                <button onClick={onClose}>CLOSE MODAL</button>
-                {children}
+                <img className="x-svg" src={svg} alt={"CLOSE"} onClick={() => onClose()}/>
+                <div className="modal-info">
+                    <p>{wing} wing, Floor {floor}</p>
+                    <p>Status: {status}</p>
+                    <p>Quality: {quality}/5</p>
+                </div>
             </div>
         </>,
         document.getElementById('portal')
