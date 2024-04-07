ce898a3a343678cd1243dd16675ff63146e0830e
BuildTools <unconfigured@null.spigotmc.org>
2024-04-07 00:50:43 -0500

    db updates

--- root/src/main.jsx
+++ root/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './index.js'
+import firebase from "firebase/compat/app";
+// Required for side-effects
+import "firebase/firestore";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
